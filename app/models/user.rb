77cd934304da458a94a9d775b78feee84212cabb
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-07 13:52:08 +0800

    Fixed incorrect spelling and indentions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     validates :name, presence: true,
-                    length: {minmum: 3,maximum: 25}
+                    length: {minimum: 3,maximum: 25}
 
     before_save {email.downcase!}
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
