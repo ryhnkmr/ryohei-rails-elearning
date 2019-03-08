2439404551aca62ce0092c2642bf95c86df98c73
ryohei0419 <47472418+ryohei0419@users.noreply.github.com>
2019-03-08 14:37:06 +0800

    created dashboard (#8)
    * created dashboard
    
    * fixed my code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def dashboard 
+  end
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
