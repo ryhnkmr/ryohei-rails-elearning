5b91060972e6e72a667ff139457d9b6d50da8dfd
ryohei0419 <47472418+ryohei0419@users.noreply.github.com>
2019-03-08 14:02:09 +0800

    created follow function (#7)
    * created follow function
    
    * delete the file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_091605) do
+ActiveRecord::Schema.define(version: 2019_03_07_113705) do
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.string "image"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
