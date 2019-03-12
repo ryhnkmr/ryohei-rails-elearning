ce4aaabb03dd3a9dce0be0a67ba7285ca8f307bf
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-12 14:52:22 +0800

    created user management function

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   delete '/logout', to: "sessions#destroy"
   get '/dashboard', to: "users#dashboard"
+  patch '/admin/users/:id', to: "admin/users#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :sessions, only:[:create]
-  resources :relationships, only:[:create, :destroy]
+  resources :relationships, only:[:create,:destroy]
 
   namespace :admin do
+    resources :users, only:[:index, :destroy]
     resources :categories do
       resources :words do
         resources :choices 
