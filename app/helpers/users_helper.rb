9b4f19e4f7071ffbaf83e049664c95a270a5d8be
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-12 15:24:16 +0800

    use helper method and delete button for current user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,14 @@ module UsersHelper
 
     image_tag(gravatar_url, alt: user.name)
   end
+
+  def admin(user)
+    if current_user != user
+      if user.admin? 
+          link_to "  remove as admin", admin_user_path(user),class: "btn btn-outline-danger float-right btn-sm " ,method: :delete 
+      else 
+          link_to "  set as admin",admin_user_path(user), class: "btn btn-outline-info float-right btn-sm ",method: :patch 
+      end   
+    end 
+  end
 end
