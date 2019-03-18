1d4eab13fd4a9933bd931e60f5a8a04051740dde
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-18 23:50:55 +0800

    created filtering function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
   def index
-    @cats = Category.paginate(page: params[:page], per_page: 6)
     @lessons = Lesson.where(user_id: current_user.id)
     @lessons_category = @lessons.collect{|n| n.category_id}
+    @status = params[:status]
+
+    all_cats = Category.paginate(page: params[:page], per_page: 6)
+    learned_cats = @lessons.collect{|n| n.category}
+
+    if @status == "all"
+      @cats = all_cats
+    elsif @status == "learned"
+      @cats = learned_cats
+    else
+      @cats = (all_cats - learned_cats)  
+    end
   end
 end
