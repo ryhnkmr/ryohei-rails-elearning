27c63ea51aed58992fd1bca5470b867e27148818
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-19 15:35:14 +0800

    fixed my design

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,15 +4,16 @@ class CategoriesController < ApplicationController
     @lessons_category = @lessons.collect{|n| n.category_id}
     @status = params[:status]
 
-    all_cats = Category.paginate(page: params[:page], per_page: 6)
+    all_cats = Category.all
     learned_cats = @lessons.collect{|n| n.category}
 
+    # abort
     if @status == "all"
-      @cats = all_cats
+      @cats = all_cats.paginate(page: params[:page], per_page: 6)
     elsif @status == "learned"
-      @cats = learned_cats
+      @cats = learned_cats.paginate(page: params[:page], per_page: 6)
     else
-      @cats = (all_cats - learned_cats)  
+      @cats = (all_cats - learned_cats).paginate(page: params[:page], per_page: 6)
     end
   end
 end
