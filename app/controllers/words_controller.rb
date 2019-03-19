f71d42622961befd0bd15edf8fe889846f2e14ec
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-19 20:59:42 +0800

    created word filtering function

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,21 @@
 class WordsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @answers = @user.answers
+    lesson = @user.lessons
+    # abort
+    @cats = lesson.collect{|n| n.category}
+    # @answers = lesson.collect{|n| n.answers}
+    @title = params[:title]
+
+    # @answers = @user.answers
+    # abort
+    # end
+    if @title == "all" || @title == nil
+      @answers = @user.answers
+      # abort 
+    else
+      lesson = Lesson.where(user_id: @user.id).where(category_id: @title)
+      @answers = lesson.collect{|n| n.answers.first}
+    end
   end
 end
