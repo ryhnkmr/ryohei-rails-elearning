25cfa8411bea29cf416b7e62e8c0d3a17fdefc21
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-19 16:24:01 +0800

    created image upload function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :search)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :search, :image)
     end
 
     def require_login
