ca63c44f9a6f2626a7ba1d9d2fa9f5ac0546215b
ryohei0419 <47472418+ryohei0419@users.noreply.github.com>
2019-03-20 14:32:22 +0800

    created word filtering function (#23)
    * created word filtering function
    
    * fixed dropdown design and removed no need code

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,15 @@
 class WordsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @answers = @user.answers
+    lesson = @user.lessons
+    @cats = lesson.collect{|n| n.category}
+    @title = params[:title]
+
+    if @title == "all" || @title == nil
+      @answers = @user.answers
+    else
+      lesson = Lesson.where(user_id: @user.id).where(category_id: @title)
+      @answers = lesson.collect{|n| n.answers.first}
+    end
   end
 end
