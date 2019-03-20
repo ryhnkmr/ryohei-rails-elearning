416db5c5d0d441a2ca9910459dd4c8333190bae9
ryohei0419 <ryouheiprograming@gmail.com>
2019-03-20 14:27:43 +0800

    fixed dropdown design and removed no need code

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,17 +2,11 @@ class WordsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     lesson = @user.lessons
-    # abort
     @cats = lesson.collect{|n| n.category}
-    # @answers = lesson.collect{|n| n.answers}
     @title = params[:title]
 
-    # @answers = @user.answers
-    # abort
-    # end
     if @title == "all" || @title == nil
       @answers = @user.answers
-      # abort 
     else
       lesson = Lesson.where(user_id: @user.id).where(category_id: @title)
       @answers = lesson.collect{|n| n.answers.first}
